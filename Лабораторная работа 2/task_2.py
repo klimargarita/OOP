16a86585c1078cce4c5780b669980123129bf6f1
klimargarita <klimova.margarita.04@gmail.com>
2025-01-31 17:10:16 +0300

    Update task_2.py
    Изменила метод

--- Лабораторная работа 2/task_2.py
+++ Лабораторная работа 2/task_2.py
@@ -27,7 +27,7 @@ class Library:
         if not self.books:
             return 1
         else:
-            return self.books[-1].id + 1
+            return max(book.id for book in self.books) + 1
 
     def get_index_by_book_id(self, book_id):
         for index, book in enumerate(self.books):
